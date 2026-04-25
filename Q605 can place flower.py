d2ba6f65c01896c4aa481c5821990f54488949fc
Devansh Khemka <khemkadevansh.dk@gmail.com>
2026-04-25 11:12:05 +0530

    Q 605

---
+++ Q605 can place flower.py
@@ -0,0 +1,16 @@
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+def canPlaceFlowers(flowerbed, n):
+    f = [0]+flowerbed+[0]
+    for i in range(1,len(f)-1):
+        if f[i-1]==0 and f[i]==0 and f[i+1]==0:
+            f[i]=1
+            n-=1
+    return n<=0
+
+n = int(input())
+flowerbed = list(map(int,input().split()))
+result = canPlaceFlowers(flowerbed, n)
+print(result)
