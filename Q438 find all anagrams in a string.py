7ec6229a254de3594ab66af2a180399459f631cf
Devansh Khemka <khemkadevansh.dk@gmail.com>
2026-05-02 15:26:25 +0530

    Q438

--- Q438 find all anagrams in a string.py
+++ Q438 find all anagrams in a string.py
@@ -1,27 +1,27 @@
 # Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 def findAnagrams(s, p):
-    res = []
-    count = [0] * 26
-
-    for c in p:
-            count[ord(c) - ord('a')] += 1
-
-    l = 0
-
-    for r in range(len(s)):
-        count[ord(s[r]) - ord('a')] -= 1
-
-        if r - l + 1 > len(p):
-            count[ord(s[l]) - ord('a')] += 1
-            l += 1
-
-        if max(count) == 0:
+    if len(p) > len(s):
+        return []
+    pcount,scount = {} ,{}
+    for i in range(len(p)):
+        pcount[p[i]] = 1 + pcount.get(p[i],0)
+        scount[s[i]] = 1 + scount.get(s[i],0)
+    res = [0] if pcount == scount else []
+    l=0
+    for r in range(len(p),len(s)):
+        scount[s[r]] = 1 + scount.get(s[r],0)
+        scount[s[l]] -= 1
+        if scount[s[l]] == 0:
+            del scount[s[l]]
+        l+=1
+        if pcount == scount:
             res.append(l)
 
     return res
 
-s = input()
-p = input()
+s = input("Enter the string s: ")
+p = input("Enter the string p: ")
+
 
 result = findAnagrams(s, p)
-print(result)
+print("Anagram start indices:", result)
